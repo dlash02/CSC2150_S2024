1ceda5830e0166ea86073db4b0b0187595cfc91e
David Lash <dlash02@gmail.com>
2024-01-14 09:48:25 -0600

    MLK Update

--- Week1/FirstPart2.java
+++ Week1/FirstPart2.java
@@ -1,6 +1,11 @@
+//import java.util.Scanner;
+
+import java.util.Scanner;
+
 public class FirstPart2 {
     public static void main(String[] args) {
-        int age = 20;
+//        int age = 20;
+        int age = getAgeFromUser();
         String rights = "";
         char cma = ',';
         if ( age == 15){
@@ -17,5 +22,21 @@ public class FirstPart2 {
             rights += cma + "Can Not Vote";
         }
         System.out.printf("Your Rights are: %s", rights);
+    }
+
+    public static int getAgeFromUser() {
+        int age = 0;
+        Scanner s = new Scanner(System.in);
+        boolean gotAge = false;
+        while ( !gotAge ) {
+            System.out.printf("\n Enter your Age(5-130) -> ");
+            age = s.nextInt();
+            if ( age >= 5 && age <=130 ){
+                gotAge = true;
+            } else {
+                System.out.printf("\n Try again Age from 5-130");
+            }
+        }
+        return age;
     }
 }
